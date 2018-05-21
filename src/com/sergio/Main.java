d13b5b0d67a802a701bb470d84fecc290db544d8
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-21 14:07:23 +0100

    Lambda Expressions Nested Blocks

--- src/com/sergio/Main.java
+++ src/com/sergio/Main.java
@@ -17,11 +17,11 @@ public class Main {
 		// 3) Body:  System.out.println("Priting from the Runnable lambda")
 		// use curly braces for code block
 		
-		new Thread(()-> {
-			System.out.println("Priting from the Runnable lambda");
-			System.out.println("Line 2");
-			System.out.format("This is line %d\n",3);
-		}).start();
+//		new Thread(()-> {
+//			System.out.println("Priting from the Runnable lambda");
+//			System.out.println("Line 2");
+//			System.out.format("This is line %d\n",3);
+//		}).start();
 		
 		Employee john = new Employee("John Doe", 30);
 		Employee tim = new Employee("Tim Buchalka", 21);
@@ -34,39 +34,46 @@ public class Main {
 		employees.add(jack);
 		employees.add(snow);
 		
-//		Collections.sort(employees, new Comparator<Employee>() {
-//
-//			@Override
-//			public int compare(Employee employee1, Employee employee2) {
-//				return employee1.getName().compareTo(employee2.getName());
-//			}
-//		});
-		
-		// we can replace the above anonymous class with lambda expression below:
-		// we can also remove the type (Employee) from the argument list, because the compiler
-		// can infer what type it is.
-		
-		Collections.sort(employees, (employee1, employee2) -> 
-				employee1.getName().compareTo(employee2.getName()));
-		
-		for(Employee employee : employees) {
-			System.out.println(employee.getName());
-		}
-		
-		
-//		String sillyString = doStringStuff(new UpperConcat() {
-//			@Override
-//			public String upperAndConcat(String s1, String s2) {
-//				return s1.toUpperCase() + s2.toUpperCase();
+////		Collections.sort(employees, new Comparator<Employee>() {
+////
+////			@Override
+////			public int compare(Employee employee1, Employee employee2) {
+////				return employee1.getName().compareTo(employee2.getName());
+////			}
+////		});
+//		
+//		// we can replace the above anonymous class with lambda expression below:
+//		// we can also remove the type (Employee) from the argument list, because the compiler
+//		// can infer what type it is.
+//		
+//		Collections.sort(employees, (employee1, employee2) -> 
+//				employee1.getName().compareTo(employee2.getName()));
+//		
+//		for(Employee employee : employees) {
+//			System.out.println(employee.getName());
 //		}
-//	}, 
-//				employees.get(0).getName(), employees.get(1).getName());
-//				System.out.println(sillyString);
+//		
+//		
+////		String sillyString = doStringStuff(new UpperConcat() {
+////			@Override
+////			public String upperAndConcat(String s1, String s2) {
+////				return s1.toUpperCase() + s2.toUpperCase();
+////		}
+////	}, 
+////				employees.get(0).getName(), employees.get(1).getName());
+////				System.out.println(sillyString);
+//		
+//		// implementation of the UpperConcat interface
+//		UpperConcat uc = (s1, s2) -> {
+//			String result = s1.toUpperCase() + s2.toUpperCase();
+//			return result;
+//		};
+//		String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+//		System.out.println(sillyString);
 		
-		// implementation of the UpperConcat interface
-		UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
-		String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
-		System.out.println(sillyString);
+		AnotherClass anotherClass = new AnotherClass();
+		String s = anotherClass.doSomething();
+		System.out.println(s);
 	}
 	
 	public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
@@ -102,4 +109,21 @@ class Employee {
 
 interface UpperConcat {
 	public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+	
+	public String doSomething() {
+		 int i = 0;
+		 
+		 UpperConcat uc = (s1,s2) -> {
+			 System.out.println("The lambda expressions class is: " + getClass().getSimpleName());
+			 String result = s1.toUpperCase() + s2.toUpperCase();
+			 return result;
+		 };
+		 
+		System.out.println("The AnotherClass class's name is: " + getClass().getSimpleName());
+		return Main.doStringStuff(uc, "String1", "String2");
+		
+	}
 }
