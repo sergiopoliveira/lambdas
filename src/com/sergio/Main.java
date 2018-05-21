a7d10083c22bf5ddae8761451d6fa10ad9143338
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-21 12:14:58 +0100

    Lambda Expressions continued

--- src/com/sergio/Main.java
+++ src/com/sergio/Main.java
@@ -34,17 +34,43 @@ public class Main {
 		employees.add(jack);
 		employees.add(snow);
 		
-		Collections.sort(employees, new Comparator<Employee>() {
-
-			@Override
-			public int compare(Employee employee1, Employee employee2) {
-				return employee1.getName().compareTo(employee2.getName());
-			}
-		});
+//		Collections.sort(employees, new Comparator<Employee>() {
+//
+//			@Override
+//			public int compare(Employee employee1, Employee employee2) {
+//				return employee1.getName().compareTo(employee2.getName());
+//			}
+//		});
+		
+		// we can replace the above anonymous class with lambda expression below:
+		// we can also remove the type (Employee) from the argument list, because the compiler
+		// can infer what type it is.
+		
+		Collections.sort(employees, (employee1, employee2) -> 
+				employee1.getName().compareTo(employee2.getName()));
 		
 		for(Employee employee : employees) {
 			System.out.println(employee.getName());
 		}
+		
+		
+//		String sillyString = doStringStuff(new UpperConcat() {
+//			@Override
+//			public String upperAndConcat(String s1, String s2) {
+//				return s1.toUpperCase() + s2.toUpperCase();
+//		}
+//	}, 
+//				employees.get(0).getName(), employees.get(1).getName());
+//				System.out.println(sillyString);
+		
+		// implementation of the UpperConcat interface
+		UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+		String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+		System.out.println(sillyString);
+	}
+	
+	public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+		return uc.upperAndConcat(s1, s2);
 	}
 }
 
@@ -72,6 +98,8 @@ class Employee {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
+}
+
+interface UpperConcat {
+	public String upperAndConcat(String s1, String s2);
 }
